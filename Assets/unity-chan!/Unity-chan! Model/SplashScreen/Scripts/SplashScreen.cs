85721bb5c5bc0aa424c08baa313d20237979a213
Takeshi Okuya <20336848+takeshi-okuya@users.noreply.github.com>
2023-08-24 13:28:11 +0900

    Add UnityChan assets

---
+++ Assets/unity-chan!/Unity-chan! Model/SplashScreen/Scripts/SplashScreen.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnityChan
+{
+	[ExecuteInEditMode]
+	public class SplashScreen : MonoBehaviour
+	{
+		void NextLevel ()
+		{
+			UnityEngine.SceneManagement.SceneManager.LoadScene(Application.loadedLevel + 1);
+			//Application.LoadLevel (Application.loadedLevel + 1);
+		}
+	}
+}
